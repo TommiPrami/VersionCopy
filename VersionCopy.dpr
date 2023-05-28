7c72cb6228a52cc6f58446130864fb3c32494f41
Tommi Prami <tommi.prami@gmail.com>
2023-05-28 10:37:48 +0300

    Added actual copy, needs some testing...

--- VersionCopy.dpr
+++ VersionCopy.dpr
@@ -5,14 +5,33 @@
 {$R *.res}
 
 uses
+  System.IOUtils,
   System.SysUtils,
   VersionUnit.Utils in 'VersionUnit.Utils.pas',
   VersionUnit.Types in 'VersionUnit.Types.pas',
   VersionUnit.Commandline.Utils in 'VersionUnit.Commandline.Utils.pas';
 
+function DoCopy(const ACopyItem: TCopyItem; var AErrorString: string): Boolean;
+begin
+  try
+    TFile.Copy(ACopyItem.SourceFullFilename, ACopyItem.DestinationFullFilename, True);
+
+    Result := True;
+  except
+    on E: Exception do
+    begin
+      Result := False;
+
+      AErrorString := Format('Exception %s occured while copying file "%s" with error message: "%S" ', [E.ClassName,
+        ACopyItem.SourceFullFilename, E.Message])
+    end;
+  end;
+end;
+
 var
   LCopyItem: TCopyItem;
   LErrorString: string;
+  LCompareResult: Integer;
 begin
   try
     if ParseCommadLine(LCopyItem, LErrorString) then
@@ -20,13 +39,14 @@ begin
       LCopyItem.SourceVersion := GetFileVersion(LCopyItem.SourceFullFilename);
       LCopyItem.DestinationVersion := GetFileVersion(LCopyItem.DestinationFullFilename);
 
-
-      var LCompareResult := CompareVersionNumbers(LCopyItem.SourceVersion, LCopyItem.DestinationVersion);
+      LCompareResult := CompareVersionNumbers(LCopyItem.SourceVersion, LCopyItem.DestinationVersion);
 
       if LCompareResult < 0 then
       begin
         WriteLn('  - Source file newer, [Source=' +  LCopyItem.SourceVersion + '] > [Destination='  + LCopyItem.DestinationVersion + ']');
 
+        if DoCopy(LCopyItem, LErrorString) then
+          WriteLn('  - Source file copied successfully to: "' + LCopyItem.DestinationPath + '"');
       end
       else if LCompareResult > 0 then
         WriteLn('  - Destination file newer, [Source=' +  LCopyItem.SourceVersion + '] < [Destination='  + LCopyItem.DestinationVersion + ']')
